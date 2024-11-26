b209d55573b97668c1d2017dde280fbcf70edc28
merrouchh <merrouchmokhtar@gmail.com>
2024-11-26 10:55:50 +0100

    Overwrite the file with updated version

--- pages/_document.js
+++ pages/_document.js
@@ -17,10 +17,18 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#000000" /> {/* Adjust the color to your app's theme */}
           <link rel="icon" href="/favicon.ico" />
           
-          {/* Apple specific meta tags for PWA */}
+          {/* Apple-specific meta tags for PWA */}
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          
+          {/* Web App Meta Tags */}
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="application-name" content="Merrouch" />
+          <meta name="apple-mobile-web-app-title" content="Merrouch" />
+          <meta name="msapplication-navbutton-color" content="#000000" />
+          <meta name="msapplication-starturl" content="/" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
           {/* Web App Manifest */}
           <link rel="manifest" href="/manifest.json" />
