7d1424d7845d1afc4e851946acab294fdcc68649
merrouchh <merrouchmokhtar@gmail.com>
2025-06-30 21:22:30 +0100

    Added standard line-clamp property for better browser compatibilitsy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,20 +19,25 @@ module.exports = {
       time: true
     },
     {
-      name: 'queue-monitor',
+      name: 'queue-monitor-realtime',
       script: './scripts/queue-monitor.js',
+      args: '--realtime',
       cwd: './',
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '500M',
+      max_memory_restart: '800M',
+      min_uptime: '10s',
+      max_restarts: 5,
+      restart_delay: 4000,
       env: {
         NODE_ENV: 'production'
       },
-      error_file: './logs/queue-monitor-error.log',
-      out_file: './logs/queue-monitor-out.log',
-      log_file: './logs/queue-monitor-combined.log',
-      time: true
+      error_file: './logs/queue-monitor-pm2-error.log',
+      out_file: './logs/queue-monitor-pm2-out.log',
+      log_file: './logs/queue-monitor-pm2-combined.log',
+      time: true,
+      merge_logs: true
     }
   ]
 }; 
